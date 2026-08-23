3b8896f21cf4946def87280ff0b14f48287cb856
Yotsakrit Reuangkaeo <67011392@kmitl.ac.th>
2026-08-23 14:14:36 +0700

    feat:restructure

--- src/main/java/com/ceiromania/pathfinder/controllers/HealthController.java
+++ src/main/java/com/ceiromania/pathfinder/controllers/HealthController.java
@@ -1,4 +1,4 @@
-package com.ceiromania.pathfinder.api;
+package com.ceiromania.pathfinder.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
