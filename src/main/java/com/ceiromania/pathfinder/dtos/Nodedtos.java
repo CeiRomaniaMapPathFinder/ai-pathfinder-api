41477d129f1198346e5e75c8e35f9eaeddf63f3f
Yotsakrit Reuangkaeo <67011392@kmitl.ac.th>
2026-09-17 19:48:49 +0700

    feature:add responseheuristicSearchDTOS

---
+++ src/main/java/com/ceiromania/pathfinder/dtos/Nodedtos.java
@@ -0,0 +1,21 @@
+package com.ceiromania.pathfinder.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Nodedtos {
+
+    private String town;
+    private int gN;
+    private int hN;
+    private int fN;
+
+    public Nodedtos(String town, int gN, int hN, int fN) {
+        this.town = town;
+        this.gN = gN;
+        this.hN = hN;
+        this.fN = fN;
+    }
+}
